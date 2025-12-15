70f27be81466ca33e61fc0d6f67027dea4fe1118
cppaddiction <132348590+cppaddiction@users.noreply.github.com>
2025-12-15 10:53:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+
+cap = cv2.VideoCapture('vid.avi')
+backSub = cv2.createBackgroundSubtractorMOG2()
+
+fourcc = cv2.VideoWriter_fourcc(*'aviv')
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+print(fps, width, height)
+
+out = cv2.VideoWriter('output_video.mp4', fourcc, fps, (width, height))
+
+while cap.isOpened():
+  # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret:
+        # Apply background subtraction
+        fg_mask = backSub.apply(frame)
+
+        # Find contours
+        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # print(contours)
+        frame_ct = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+
+        # apply global threshold to remove shadows
+        retval, mask_thresh = cv2.threshold(fg_mask, 180, 255, cv2.THRESH_BINARY)
+
+        # set the kernel
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+        # Apply erosion
+        mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
+
+        min_contour_area = 500  # Define your minimum area threshold
+        large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
+
+        frame_out = frame.copy()
+        for cnt in large_contours:
+            x, y, w, h = cv2.boundingRect(cnt)
+            frame_out = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 200), 3)
+
+        out.write(frame_out)
